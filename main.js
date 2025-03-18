feb66a95a3b24bd5a56caaaba670ff2219055f64
Romain Pierre Marie Denis <ps04egl@eduvaud.ch>
2025-03-18 16:50:57 +0100

    Ex8

--- main.js
+++ main.js
@@ -9,11 +9,22 @@ const app = Vue.createApp({
       url: "https://www.etml.ch/",
       //inStock: true,
       inventory: 0,
+      selectedVariant: 0,
       onSale: true,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 1, color: "Green", image: "./assets/images/socks_green.jpg" },
-        { id: 2, color: "Blue", image: "./assets/images/socks_blue.jpg" },
+        {
+          id: 1,
+          quantity: 0,
+          color: "Green",
+          image: "./assets/images/socks_green.jpg",
+        },
+        {
+          id: 2,
+          quantity: 50,
+          color: "Blue",
+          image: "./assets/images/socks_blue.jpg",
+        },
       ],
       sizes: [
         { id: 3, size: "S" },
@@ -22,6 +33,7 @@ const app = Vue.createApp({
         { id: 6, size: "XL" },
       ],
       cart: 0,
+      brand: "Vue Mastery",
     };
   },
   methods: {
@@ -34,5 +46,20 @@ const app = Vue.createApp({
     updateImage(variantImage) {
       this.image = variantImage;
     },
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return ??
+    },
+    inStock() {
+      return ??
+    }
+
   },
 });
