e281d4128f2593c0a04232aeaac93435fd652f1e
Romain Pierre Marie Denis <ps04egl@eduvaud.ch>
2025-03-18 15:48:32 +0100

    Ex6
    For
    Methods

--- main.js
+++ main.js
@@ -11,6 +11,28 @@ const app = Vue.createApp({
       inventory: 0,
       onSale: true,
       details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 1, color: "Green", image: "./assets/images/socks_green.jpg" },
+        { id: 2, color: "Blue", image: "./assets/images/socks_blue.jpg" },
+      ],
+      sizes: [
+        { id: 3, size: "S" },
+        { id: 4, size: "M" },
+        { id: 5, size: "L" },
+        { id: 6, size: "XL" },
+      ],
+      cart: 0,
     };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    removeToCart() {
+      this.cart -= 1;
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
   },
 });
