cf2f2d79dfcfa7eca359077566ec33d68af7931f
Romain Pierre Marie Denis <ps04egl@eduvaud.ch>
2025-03-26 11:13:59 +0100

    CartDisplay
    Total doesn't work yet but dw

---
+++ MiseEnPratique/components/CartDisplay.js
@@ -0,0 +1,22 @@
+app.component("cart-display", {
+  props: ["cart"],
+  template:
+    /*html*/
+    `
+    <h2>🛒 Mon Panier {{ cart.length }}</h2>
+        <ul>
+          <li v-for="gadget in cart">
+            {{gadget.nom}} - {{gadget.prix}}
+            <button class="remove-btn" @click="removeFromCart(gadget)">
+              ❌
+            </button>
+          </li>
+          <p>Total: {{total}}</p>
+        </ul>
+        <p v-if="cart.length <= 0">Votre panier est vide.</p>`,
+  methods: {
+    removeFromCart(gadget) {
+      this.$emit("removeFromCart", gadget);
+    },
+  },
+});
