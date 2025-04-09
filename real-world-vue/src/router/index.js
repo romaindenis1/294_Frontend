a65a8faa066b83b664ae45082507838daccc52c3
Romain Pierre Marie Denis <ps04egl@eduvaud.ch>
2025-04-09 09:06:02 +0200

    Finish(ex5)

--- real-world-vue/src/router/index.js
+++ real-world-vue/src/router/index.js
@@ -1,19 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import EventListView from '../views/EventListView.vue'
-import AboutView from '../views/AboutView.vue'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'event-list',
-      component: EventListView,
+      name: 'home',
+      component: HomeView,
     },
     {
       path: '/about',
       name: 'about',
-      component: AboutView,
+      component: () => import('../views/AboutView.vue'),
     },
   ],
 })
